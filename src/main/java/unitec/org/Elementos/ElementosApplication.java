d1dd06fa6c22c02d5fbb4cea17a116d30723af5d
T-107 <T-107@PC199>
2018-01-30 21:38:43 -0600

    Clase 30/01/18

--- src/main/java/unitec/org/Elementos/ElementosApplication.java
+++ src/main/java/unitec/org/Elementos/ElementosApplication.java
@@ -1,5 +1,6 @@
 package unitec.org.Elementos;
 
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,6 +17,22 @@ public class ElementosApplication implements CommandLineRunner{
     @Override
     public void run(String... strings) throws Exception {
         Mensajitos n=new Mensajitos("primero","Mi primer mensajito con Hibernate");
-        repoMensa.save(n);
+        //repoMensa.save(n);
+        
+        //Buscar Todos
+        ArrayList<Mensajitos> mensajitos = (ArrayList<Mensajitos>)repoMensa.findAll();
+        for (Mensajitos mensa: mensajitos){
+            System.out.println(mensa.getTitulo());
+        }
+        
+        // Ahora probaremos de nuevo buscar por ID
+        Mensajitos m1 = repoMensa.findOne(1);
+        System.out.println(m1.getTitulo());
+        
+        // Actualización
+        //repoMensa.save(new Mensajitos(1,"otro","malo"));
+        
+        //Borrar
+        repoMensa.delete(1);
     }
 }
